e1a51057547de78049d720a6734a31eccce3f688
Michael Davis <mcd164@miami.edu>
2018-09-07 16:55:44 -0400

    Remove logging

--- lib/plug/parsers/xmlrpc.ex
+++ lib/plug/parsers/xmlrpc.ex
@@ -7,7 +7,6 @@ defmodule Plug.Parsers.XMLRPC do
   """
 
   def init(opts) do
-    IO.inspect(opts)
     {decoder, opts} = Keyword.pop(opts, :xmlrpc_decoder)
 
     unless decoder do
@@ -15,11 +14,9 @@ defmodule Plug.Parsers.XMLRPC do
     end
 
     {decoder, opts}
-    |> IO.inspect
   end
 
   def parse(conn, _type, "xml", _headers, {decoder, opts}) do
-    IO.puts("In the right parse block")
     conn
     |> read_body(opts)
     |> decode(decoder)
