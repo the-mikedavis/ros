a1606731a7ddb53f1892cf58aefd03aafb875c22
Michael Davis <mcd164@miami.edu>
2018-09-07 18:37:48 -0400

    Create a shell for the option parser

---
+++ lib/ros/option_parser.ex
@@ -0,0 +1,8 @@
+defmodule ROS.OptionParser do
+  @moduledoc """
+  An extension of the Elixir `OptionParser` for remapping command line arguments
+  for ROS nodes.
+  """
+
+  @type argv :: [String.t()]
+end
