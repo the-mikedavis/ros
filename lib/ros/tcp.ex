86a8cb57af37bfc6438f00c2e4ad18f62df589cc
Michael Davis <mcd164@miami.edu>
2018-09-14 00:01:12 -0400

    Subscribe succesfully

--- lib/ros/tcp.ex
+++ lib/ros/tcp.ex
@@ -13,14 +13,14 @@ defmodule ROS.TCP do
 
   @impl GenServer
   def handle_call(:accept, _from, state) do
-    {:ok, port} =
+    {:ok, socket} =
       :gen_tcp.listen(0, [:binary, reuseaddr: true, active: true, packet: 0])
 
-    {:ok, port_number} = :inet.port(port)
+    {:ok, port_number} = :inet.port(socket)
 
-    GenServer.cast(self(), {:accept, port})
+    GenServer.cast(self(), {:accept, socket})
 
-    {:reply, port_number, state}
+    {:reply, port_number, Map.put(state, :socket, socket)}
   end
 
   @impl GenServer
@@ -31,9 +31,24 @@ defmodule ROS.TCP do
       |> Enum.map(&String.to_integer/1)
       |> List.to_tuple()
 
-    {:ok, socket} = :gen_tcp.connect(ip_addr, port, packet: 0)
+    {:ok, socket} = :gen_tcp.connect(ip_addr, port, [:binary, packet: 0])
 
     :ok = :gen_tcp.controlling_process(socket, self())
+
+    data =
+      <<178, 0, 0, 0, 37, 0, 0, 0, 99, 97, 108, 108, 101, 114, 105, 100, 61, 47, 108,
+      105, 115, 116, 101, 110, 101, 114, 95, 57, 52, 56, 57, 95, 49, 53, 51, 54, 54,
+      50, 54, 51, 55, 53, 53, 55, 52, 39, 0, 0, 0, 109, 100, 53, 115, 117, 109, 61,
+      57, 57, 50, 99, 101, 56, 97, 49, 54, 56, 55, 99, 101, 99, 56, 99, 56, 98, 100,
+      56, 56, 51, 101, 99, 55, 51, 99, 97, 52, 49, 100, 49, 31, 0, 0, 0, 109, 101,
+      115, 115, 97, 103, 101, 95, 100, 101, 102, 105, 110, 105, 116, 105, 111, 110,
+      61, 115, 116, 114, 105, 110, 103, 32, 100, 97, 116, 97, 10, 13, 0, 0, 0, 116,
+      99, 112, 95, 110, 111, 100, 101, 108, 97, 121, 61, 48, 14, 0, 0, 0, 116, 111,
+      112, 105, 99, 61, 47, 99, 104, 97, 116, 116, 101, 114, 20, 0, 0, 0, 116, 121,
+      112, 101, 61, 115, 116, 100, 95, 109, 115, 103, 115, 47, 83, 116, 114, 105,
+      110, 103>>
+
+    GenServer.cast(self(), {:send, data})
 
     {:noreply, Map.put(state, :socket, socket)}
   end
@@ -44,13 +59,21 @@ defmodule ROS.TCP do
     {:noreply, Map.put(state, :socket, socket)}
   end
 
-  def handle_cast({:send, _data}, state) do
-    # TODO: serialize data and send
+  def handle_cast({:send, data}, %{socket: socket} = state) do
+    :gen_tcp.send(socket, data)
 
     {:noreply, state}
   end
 
   @impl GenServer
+  # for subscribers, send the connection header and then get data
+  def handle_info({:tcp, _socket, packet}, %{callback: callback} = state) do
+    callback.(packet)
+
+    {:noreply, state}
+  end
+
+  # for publishers, parse the connection header
   def handle_info({:tcp, _socket, packet}, state) do
     packet
     |> ROS.Message.parse()
