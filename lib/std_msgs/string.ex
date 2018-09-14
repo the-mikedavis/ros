0d8cd6e2c8462ca580fb27ccafa400d7f78c4444
Michael Davis <mcd164@miami.edu>
2018-09-14 10:03:49 -0400

    Manually create std_msgs/String

---
+++ lib/std_msgs/string.ex
@@ -0,0 +1,21 @@
+defmodule StdMsgs.String do
+  @moduledoc """
+  The String type of std_msgs.
+
+  The definition:
+
+  ```
+  string data
+  ```
+  """
+
+  @type t :: %__MODULE__{data: String.t()}
+
+  defstruct [data: ""]
+
+  @spec md5sum() :: String.t()
+  def md5sum, do: "992ce8a1687cec8c8bd883ec73ca41d1"
+
+  @spec name() :: String.t()
+  def name, do: "std_msgs/String"
+end
