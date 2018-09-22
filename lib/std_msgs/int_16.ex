7ae64fabe5de80f48b33de85c1bf167edac49345
Michael Davis <mcd164@miami.edu>
2018-09-22 15:40:40 -0400

    Play around with floats and ints

---
+++ lib/std_msgs/int_16.ex
@@ -0,0 +1,29 @@
+defmodule StdMsgs.Int16 do
+  @behaviour ROS.Message.Behaviour
+
+  @moduledoc """
+  The Int16 type of std_msgs.
+
+  The definition:
+
+  ```
+  int16 data
+  ```
+  """
+
+  @type t :: %__MODULE__{
+    data: integer()
+  }
+
+  defstruct [data: 0]
+
+  @impl ROS.Message.Behaviour
+  def md5sum, do: "8524586e34fbd7cb1c08c5f5f1ca0e57"
+
+  @impl ROS.Message.Behaviour
+  def definition do
+    """
+    int16 data
+    """
+  end
+end
