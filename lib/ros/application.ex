d4366e6f671953e4ef688804b9a4cb966da2a1e9
Michael Davis <mcd164@miami.edu>
2018-11-02 17:12:59 -0400

    Tests with service server

--- lib/ros/application.ex
+++ lib/ros/application.ex
@@ -4,22 +4,22 @@ defmodule ROS.Application do
   # TODO: remove once this is reproducible
 
   use Application
+  require Logger
   import ROS.Node.Spec
 
   def start(_type, _args) do
-    _add_two_ints = fn x, y ->
-      IO.inspect(x, label: "x")
-      IO.inspect(y, label: "y")
+    add_two_ints = fn %RospyTutorials.AddTwoInts.Request{a: a, b: b} ->
+      Logger.debug(fn -> "[#{a} + #{b} = #{a + b}]" end)
 
-      %RospyTutorials.AddTwoInts.Response{sum: x + y}
+      %RospyTutorials.AddTwoInts.Response{sum: a + b}
     end
 
     children = [
       # node(:"/mynode", [publisher(:talker, "/chatter", "std_msgs/Int16")])
       node(:"/mynode", [
         #subscriber("/chatter", "std_msgs/Int32MultiArray", &IO.inspect/1)
-        service_proxy(:proximus, "/add_two_ints", "rospy_tutorials/AddTwoInts")
-        # service("/add_two_ints", "rospy_tutorials/AddTwoInts", add_two_ints)
+        # service_proxy(:proximus, "/add_two_ints", "rospy_tutorials/AddTwoInts")
+        service("/add_two_ints", "rospy_tutorials/AddTwoInts", add_two_ints)
       ])
     ]
 
